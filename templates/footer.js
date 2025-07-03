1dad8e8573dcc98b0e07977c519827b93bac0412
Sue Smith <sue@benormal.info>
2025-07-03 15:38:46 +0000

    publish to compute

--- templates/footer.js
+++ templates/footer.js
@@ -12,7 +12,7 @@ export const renderFooter = html.node`<footer class="footer">
     href="https://glitch.com/edit/#!/remix/glitch-in-bio"
   >
     <img
-      src="https://cdn.glitch.com/605e2a51-d45f-4d87-a285-9410ad350515%2FLogo_Color.svg?v=1618199565140"
+      src="/605e2a51-d45f-4d87-a285-9410ad350515_Logo_Color.svg"
       alt=""
     />
     Get your own free page
